7eab8f6742d947b2c129cc6d3e7a4a2075d888cd
danila.khartsy <danila.khartsy@astrum.team>
2025-10-08 18:46:07 +0300

    set task 4

--- hw2/task4/task4.py
+++ hw2/task4/task4.py
@@ -2,14 +2,12 @@ try:
     with open("input.txt", "r", encoding="utf-8") as file:
         lines = file.readlines()
 
-    lines = [line.strip() for line in lines]
-
-    unique_once = [line for line in lines if lines.count(line) == 1]
+    unique_lines = set(line.strip() for line in lines)
 
     with open("unique_output.txt", "w", encoding="utf-8") as file:
-        for line in unique_once:
+        for line in unique_lines:
             file.write(line + "\n")
 
-    print("Строки, встречающиеся один раз, записаны в unique_output.txt.")
+    print("Уникальные строки успешно записаны в unique_output.txt.")
 except FileNotFoundError:
     print("Ошибка: файл input.txt не найден.")
