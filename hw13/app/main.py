e2723b5dc20d7dcf968abbedcab2f697cbe9668f
dkhartsiy <danila.khartsiy@s-telematics.ru>
2025-11-27 08:18:26 +0300

    hw13: tests endpoints

---
+++ hw13/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from .database import Base, engine
+from .auth import router as auth_router
+from .students import router as students_router
+from .tasks import router as tasks_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Students API with Auth, BackgroundTasks & Redis Cache")
+
+app.include_router(auth_router)
+app.include_router(students_router)
+app.include_router(tasks_router)
---
+++ hw13/tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+from app.database import Base, engine, SessionLocal
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+@pytest.fixture
+def db():
+    return SessionLocal()
